0003216139ca33d9568ba44b95748af7a30d528d
Connor <connordriza@gmail.com>
2024-03-04 23:01:39 -0800

    made transform function math-based & way less ugly

--- md5.lua
+++ md5.lua
@@ -286,92 +286,64 @@ local CONSTS = {
   0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476
 }
 
-local f=function (x,y,z) return bit_or(bit_and(x,y),bit_and(-x-1,z)) end
-local g=function (x,y,z) return bit_or(bit_and(x,z),bit_and(y,-z-1)) end
-local h=function (x,y,z) return bit_xor(x,bit_xor(y,z)) end
-local i=function (x,y,z) return bit_xor(y,bit_or(x,-z-1)) end
+local md5_transform_funcs = {
+    [1] = function(x, y, z) return bit.bor(bit.band(x, y), bit.band(-x - 1, z)) end,
+    [2] = function(x, y, z) return bit.bor(bit.band(x, z), bit.band(y, -z - 1)) end,
+    [3] = function(x, y, z) return bit.bxor(x, bit.bxor(y, z)) end,
+    [4] = function(x, y, z) return bit.bxor(y, bit.bor(x, -z - 1)) end
+}
+
 local z=function (ff,a,b,c,d,x,s,ac)
   a=bit_and(a+ff(b,c,d)+x+ac,0xFFFFFFFF)
   -- be *very* careful that left shift does not cause rounding!
   return bit_or(bit_lshift(bit_and(a,bit_rshift(0xFFFFFFFF,s)),s),bit_rshift(a,32-s))+b
 end
 
-local function transform(A,B,C,D,X)
-  local a,b,c,d=A,B,C,D
-  local t=CONSTS
+local md5_sTab = {
+    {7, 12, 17, 22},
+    {5, 9, 14, 20},
+    {4, 11, 16, 23},
+    {6, 10, 15, 21},
+}
 
-  a=z(f,a,b,c,d,X[ 0], 7,t[ 1])
-  d=z(f,d,a,b,c,X[ 1],12,t[ 2])
-  c=z(f,c,d,a,b,X[ 2],17,t[ 3])
-  b=z(f,b,c,d,a,X[ 3],22,t[ 4])
-  a=z(f,a,b,c,d,X[ 4], 7,t[ 5])
-  d=z(f,d,a,b,c,X[ 5],12,t[ 6])
-  c=z(f,c,d,a,b,X[ 6],17,t[ 7])
-  b=z(f,b,c,d,a,X[ 7],22,t[ 8])
-  a=z(f,a,b,c,d,X[ 8], 7,t[ 9])
-  d=z(f,d,a,b,c,X[ 9],12,t[10])
-  c=z(f,c,d,a,b,X[10],17,t[11])
-  b=z(f,b,c,d,a,X[11],22,t[12])
-  a=z(f,a,b,c,d,X[12], 7,t[13])
-  d=z(f,d,a,b,c,X[13],12,t[14])
-  c=z(f,c,d,a,b,X[14],17,t[15])
-  b=z(f,b,c,d,a,X[15],22,t[16])
+local md5_xTab = {
+    {1, 6, 11, 0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12},
+    {5, 8, 11, 14, 1, 4, 7, 10, 13, 0, 3, 6, 9, 12, 15, 2},
+    {0, 7, 14, 5, 12, 3, 10, 1, 8, 15, 6, 13, 4, 11, 2, 9}
+}
 
-  a=z(g,a,b,c,d,X[ 1], 5,t[17])
-  d=z(g,d,a,b,c,X[ 6], 9,t[18])
-  c=z(g,c,d,a,b,X[11],14,t[19])
-  b=z(g,b,c,d,a,X[ 0],20,t[20])
-  a=z(g,a,b,c,d,X[ 5], 5,t[21])
-  d=z(g,d,a,b,c,X[10], 9,t[22])
-  c=z(g,c,d,a,b,X[15],14,t[23])
-  b=z(g,b,c,d,a,X[ 4],20,t[24])
-  a=z(g,a,b,c,d,X[ 9], 5,t[25])
-  d=z(g,d,a,b,c,X[14], 9,t[26])
-  c=z(g,c,d,a,b,X[ 3],14,t[27])
-  b=z(g,b,c,d,a,X[ 8],20,t[28])
-  a=z(g,a,b,c,d,X[13], 5,t[29])
-  d=z(g,d,a,b,c,X[ 2], 9,t[30])
-  c=z(g,c,d,a,b,X[ 7],14,t[31])
-  b=z(g,b,c,d,a,X[12],20,t[32])
+local md5_setOrder = {
+    1,
+    4,
+    3,
+    2
+}
 
-  a=z(h,a,b,c,d,X[ 5], 4,t[33])
-  d=z(h,d,a,b,c,X[ 8],11,t[34])
-  c=z(h,c,d,a,b,X[11],16,t[35])
-  b=z(h,b,c,d,a,X[14],23,t[36])
-  a=z(h,a,b,c,d,X[ 1], 4,t[37])
-  d=z(h,d,a,b,c,X[ 4],11,t[38])
-  c=z(h,c,d,a,b,X[ 7],16,t[39])
-  b=z(h,b,c,d,a,X[10],23,t[40])
-  a=z(h,a,b,c,d,X[13], 4,t[41])
-  d=z(h,d,a,b,c,X[ 0],11,t[42])
-  c=z(h,c,d,a,b,X[ 3],16,t[43])
-  b=z(h,b,c,d,a,X[ 6],23,t[44])
-  a=z(h,a,b,c,d,X[ 9], 4,t[45])
-  d=z(h,d,a,b,c,X[12],11,t[46])
-  c=z(h,c,d,a,b,X[15],16,t[47])
-  b=z(h,b,c,d,a,X[ 2],23,t[48])
+function transform(A, B, C, D, X)
+    local tab = {A, B, C, D};
+    local argsOrder = {1, 2, 3, 4}
+    local function carousel()
+        local tmp = {}
+        tmp[1] = argsOrder[4]
+        tmp[2] = argsOrder[1]
+        tmp[3] = argsOrder[2]
+        tmp[4] = argsOrder[3]
+        return tmp;
+    end
 
-  a=z(i,a,b,c,d,X[ 0], 6,t[49])
-  d=z(i,d,a,b,c,X[ 7],10,t[50])
-  c=z(i,c,d,a,b,X[14],15,t[51])
-  b=z(i,b,c,d,a,X[ 5],21,t[52])
-  a=z(i,a,b,c,d,X[12], 6,t[53])
-  d=z(i,d,a,b,c,X[ 3],10,t[54])
-  c=z(i,c,d,a,b,X[10],15,t[55])
-  b=z(i,b,c,d,a,X[ 1],21,t[56])
-  a=z(i,a,b,c,d,X[ 8], 6,t[57])
-  d=z(i,d,a,b,c,X[15],10,t[58])
-  c=z(i,c,d,a,b,X[ 6],15,t[59])
-  b=z(i,b,c,d,a,X[13],21,t[60])
-  a=z(i,a,b,c,d,X[ 4], 6,t[61])
-  d=z(i,d,a,b,c,X[11],10,t[62])
-  c=z(i,c,d,a,b,X[ 2],15,t[63])
-  b=z(i,b,c,d,a,X[ 9],21,t[64])
+    for i = 0, 63 do
+        local chunk = math.floor(i / 16) + 1;
+        local xIndex = (i <= 15) and i or md5_xTab[chunk - 1][(i % 16) + 1];
+        local x = X[xIndex];
+        local current = (i % 4) + 1;
+        local s = md5_sTab[chunk][current];
+        tab[md5_setOrder[current]] = z(md5_transform_funcs[chunk], tab[argsOrder[1]], tab[argsOrder[2]], tab[argsOrder[3]], tab[argsOrder[4]], x, s, CONSTS[i + 1])
+        argsOrder = carousel();
+    end
 
-  return bit_and(A+a,0xFFFFFFFF),bit_and(B+b,0xFFFFFFFF),
-         bit_and(C+c,0xFFFFFFFF),bit_and(D+d,0xFFFFFFFF)
+    return bit_and(A+tab[1],0xFFFFFFFF),bit_and(B+tab[2],0xFFFFFFFF),
+         bit_and(C+tab[3],0xFFFFFFFF),bit_and(D+tab[4],0xFFFFFFFF)
 end
-
 ----------------------------------------------------------------
 
 local function md5_update(self, s)
